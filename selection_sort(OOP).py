a000c911a0c7b500878109fceac4afb753c8fb42
Hemant <68590578+Hemant-29@users.noreply.github.com>
2023-01-15 18:21:03 +0530

    Update selection_sort(OOP).py

--- selection_sort(OOP).py
+++ selection_sort(OOP).py
@@ -6,14 +6,13 @@ print("original list: ",list)
 
 class sort():
 	def __init__(self,list,type=""):
-	self.indx=0
+		self.indx=0
+		self.list=list
 		if "select" in type.lower():
 			for i in range(len(list)):
-				selction_sort()
+				self.selection_sort(self.indx)
 
-	def selection_sort(self):
-		indx=self.indx
-		list=self.list()
+	def selection_sort(self,indx):
 		min=10**100
 		#finding minimum
 		for i in range(indx,len(list)):
@@ -23,8 +22,8 @@ class sort():
 		#swapping
 		list[indx_min] = list[indx]
 		list[indx]=min
-		indx+=1
+		self.indx+=1
 
-
+sort(list,"selection")
 print("sorted list: ",end="")
 print(list)
