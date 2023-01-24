cefdb4e277e36c4bba68bf104664e2426d9feb30
Hemant <68590578+Hemant-29@users.noreply.github.com>
2023-01-25 00:30:52 +0530

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,18 +1,17 @@
-list=[29,51,24,57,99,32,67,240,4,79,8,5,32,1]
-#sorting the list
+list = [29, 51, 24, 57, 99, 32, 67, 240, 4, 79, 8, 5, 32, 1]
+print("original list:", list)
+# sorting the list
 def sort():
 
-	
-	
-	global sorted_list,list
-	for i in range(len(list)-1):
-		n1,n2=list[i],list[i+1]
-		if n1>n2:
-			list[i+1]=n1
-			list[i]=n2
-			
+    global sorted_list, list
+    for i in range(len(list) - 1):
+        n1, n2 = list[i], list[i + 1]
+        if n1 > n2:
+            list[i + 1] = n1
+            list[i] = n2
+
 
 for i in range(len(list)):
-	sort()
-
+    sort()
+print("sorted list: ", end="")
 print(list)
