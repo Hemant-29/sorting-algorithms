cc10021bf537cd229a06801f13576a19b867b2fe
Hemant <68590578+Hemant-29@users.noreply.github.com>
2023-01-26 12:55:14 +0530

    Update selection_sort(OOP).py

--- selection_sort(OOP).py
+++ selection_sort(OOP).py
@@ -1,29 +1,29 @@
-list=[71,91,46,61,92,89,58,10,4,2,40,97]
-print("original list: ",list)
+list = [71, 91, 46, 61, 92, 89, 58, 10, 4, 2, 40, 97]
 
-#sorting the list
+# sorting the list
 
 
-class sort():
-	def __init__(self,list,type=""):
-		self.indx=0
-		self.list=list
-		if "select" in type.lower():
-			for i in range(len(list)):
-				self.selection_sort(self.indx)
+class sort:
+    def __init__(self, list, type=""):
+        self.indx = 0
+        self.list = list
+        if "select" in type.lower():
+            for i in range(len(list)):
+                self.selection_sort(self.indx)
 
-	def selection_sort(self,indx):
-		min=10**100
-		#finding minimum
-		for i in range(indx,len(list)):
-			if list[i] < min:
-				min = list[i]
-				indx_min = i
-		#swapping
-		list[indx_min] = list[indx]
-		list[indx]=min
-		self.indx+=1
+    def selection_sort(self, indx):
+        min = 10**100
+        # finding minimum
+        for i in range(indx, len(list)):
+            if list[i] < min:
+                min = list[i]
+                indx_min = i
+        # swapping
+        list[indx_min] = list[indx]
+        list[indx] = min
+        self.indx += 1
 
-sort(list,"selection")
-print("sorted list: ",end="")
+
+sort(list, "selection")
+print("sorted list: ", end="")
 print(list)
