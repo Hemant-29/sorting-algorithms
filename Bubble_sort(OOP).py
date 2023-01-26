5e5de9332b11a8981f4f73b844abc91d598fbb67
Hemant <68590578+Hemant-29@users.noreply.github.com>
2023-01-26 12:53:31 +0530

    bugs fixed

--- Bubble_sort(OOP).py
+++ Bubble_sort(OOP).py
@@ -1,33 +1,33 @@
-list=[29,51,24,57,99,32,67,240,4,79,8,5,32,1]
+list = [29, 51, 24, 57, 99, 32, 67, 240, 4, 79, 8, 5, 32, 1]
 
-#sorting the list
+# sorting the list
 
-class sort():
 
-	def __init__(self,list,type=""):		self.list=list
+class sort:
+    def __init__(self, list, type=""):
+        self.list = list
 
-		if "bubble" in type.lower():
+        if "bubble" in type.lower():
 
-			for i in range(len(list)):
+            for i in range(len(list)):
 
-				self.bubble_sort()
+                self.bubble_sort()
 
-		
+    def bubble_sort(self):
 
-	def bubble_sort(self):
+        for i in range(len(list) - 1):
 
-		for i in range(len(list)-1):
+            n1 = list[i]
 
-			n1=list[i]
+            n2 = list[i + 1]
 
-			n2=list[i+1]
+            if n1 > n2:
 
-			if n1>n2:
+                list[i + 1] = n1
 
-				list[i+1]=n1
+                list[i] = n2
 
-				list[i]=n2
 
-sort(list,"BuBBle")
+sort(list, "BuBBle")
 
 print(list)
