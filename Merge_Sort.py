957da7e6a3b21d9aa7111b6aa80ec3716e697ded
Hemant <68590578+Hemant-29@users.noreply.github.com>
2023-02-04 00:25:54 +0530

    Add files via upload

---
+++ Merge_Sort.py
@@ -0,0 +1,49 @@
+#FORMING LIST
+import random,time
+strt=time.time()
+#creating list
+list=[]
+for i in range(10**4):
+	a=random.randrange(10**4)
+	if a not in list:
+		list.append(a)
+print("list is created")
+print(f"list has {len(list)} elements in it.")
+#list=[27,2,90,43,51,22]
+
+#DIVIDE FUNCTION#
+def divide(list):
+	mid=len(list)//2
+	L=list[:mid]
+	R=list[mid:]
+	if len(list)==1:
+		return list
+	elif len(list)==2:
+		return merge(L,R)
+	elif len(list)>2:
+		out=merge(divide(L),divide(R))
+		return out
+
+#MERGE FUNCTION#
+def merge(list1,list2):
+	list=[]
+	while len(list1)>0 and len(list2)>0:
+		e1=list1[0]
+		e2=list2[0]
+		if e1<e2:
+			list.append(e1)
+			del list1[0]
+		else:
+			list.append(e2)
+			del list2[0]
+	while not (len(list1)==0 and len(list2)==0):
+		for i in [list1,list2]:
+			if len(i)!=0:
+				list.append(i[0])
+				del i[0]
+	return(list)
+
+#CALLING MAIN FUNCTION#
+print(divide(list))
+end=time.time()
+print("time taken is",end-strt)
